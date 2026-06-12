114e60ddc18689263afd2ababcd5055f236e032e
Devraj Singh Shekhawat <theraaj14@gmail.com>
2026-06-12 13:30:55 +0530

    Added an new file src/learning1.md and modified routes sequence. The learning now will be going to added in the file src/learning.md and modified src/app.js.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
+
+app.use('/test',function(req,res){
+    res.send(" Hello from test route :)");
+})
+
+
+app.use('/hello',function(req,res){
+    res.send("Hello Hello Hello :)")
+})
+
 app.use('/',(req,res)=>{
     res.end("Jai Shri krishna");
 })
+
 app.listen(7777,()=>{
     console.log("Jai jagannath ji")
     console.log("http://localhost:7777");
---
+++ src/learning1.md
@@ -0,0 +1,3 @@
+# Learning of NodeJs and Backend Development
+- app.use() can be also use to create route.
+- The order of all the route matters because if an route string is like "/" and on top then all the other routes start with the same string like "/hello","/test","/signup" ,etc give the route handler of that first route with string "/".
